fe02d844b72d416d2e71bd41c41453ab860a389c
java <deepan2104d@gmail.com>
2021-11-10 08:32:07 +0530

    msg

---
+++ src/base/newname.java
@@ -0,0 +1,13 @@
+package base;
+
+public class newname {
+	public static void main(String[] args) {
+		String s="java programing";
+		for (int i = s.length()-1; i >=0; i--) {
+			char charAt = s.charAt(i);
+		
+			System.out.print(charAt);
+		}
+	}
+
+}
